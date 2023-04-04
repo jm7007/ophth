51b181eba9f1819bdc4cc7eddc47a7b8ee35df75
JongMin <jongminleg@gmail.com>
2023-04-04 09:23:08 +0900

    tmp

--- src/main/resources/static/js/reservation.js
+++ src/main/resources/static/js/reservation.js
@@ -2,9 +2,9 @@
 
 function makeCalendar(tableId) {
     const calendar = document.getElementById(tableId);
-    for(let e of ["일","월","화","수","목","금","토"]){
+    for (let e of ["일", "월", "화", "수", "목", "금", "토"]) {
         let th = document.createElement("th");
-        th.innerHTML=e;
+        th.innerHTML = e;
         calendar.appendChild(th);
     }
     let tr;
@@ -13,17 +13,17 @@ function makeCalendar(tableId) {
     let tdId;
     for (let i = 1; i <= 42; i++) {
         if (i % 7 == 1) {
-            if(i!=1){
+            if (i != 1) {
                 calendar.appendChild(tr);
             }
             tr = document.createElement("tr");
-            trId = tableId+"-"+(Math.ceil(i/7));
-            tr.setAttribute("id",trId);
+            trId = tableId + "-r-" + (Math.ceil(i / 7));
+            tr.setAttribute("id", trId);
         }
         td = document.createElement("td");
-        tdId = trId+"-"+i;
-        td.setAttribute("id",tdId);
-        td.innerHTML=i;
+        tdId = tableId + "-d-" + i;
+        td.setAttribute("id", tdId);
+        td.innerHTML = i;
         tr.appendChild(td);
         if (i == 42) {
             calendar.appendChild(tr);
@@ -142,4 +142,4 @@ const showRSwithCal = (function (reservations) {
             container.appendChild(hourlabel);
         }
     });
-})(reservations);
+});
